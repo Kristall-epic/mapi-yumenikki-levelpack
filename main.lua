da72c0bae4b7d2d95d7196d8fd00b8a325f11412
Kristall <100389009+Kristall-epic@users.noreply.github.com>
2026-03-22 13:57:06 -0400

    gain 2 byte

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 -- name: [MAP] Yume Nikki Level Pack
 
-if _G.MAPi_Active then
+if MAPi_Active then
   --adds a new hangout to mapi
-  hangout_madotsuki = _G.MAPi.hangout_map_add(
+  hangout_madotsuki = MAPi.hangout_map_add(
     LEVEL_MADO, 
     "Madotsuki's Room",
     "The room of the protagonist from the game Yume Nikki!",
@@ -12,10 +12,10 @@ if _G.MAPi_Active then
     )
     
   --adds an entry sound to the hangout
-  _G.MAPi.hangout_add_entry_sound(hangout_madotsuki, madoenter)
+  MAPi.hangout_add_entry_sound(hangout_madotsuki, madoenter)
   
   --adds a skybox to the hangout, it will complete a full rotation in 240 seconds
-  _G.MAPi.hangout_add_skybox(hangout_madotsuki, {
+  MAPi.hangout_add_skybox(hangout_madotsuki, {
     skytype = "ico",
     front = madoSkies[1]
   }, 240)
@@ -23,10 +23,10 @@ if _G.MAPi_Active then
   MAPi.hangout_edit_text_color(hangout_madotsuki, {r = 0xD2, g = 0x54, b = 0x69})
   
   --adds an environment tint so it can be modified later
-  _G.MAPi.hangout_add_env_tint(hangout_madotsuki, {r = 255, g = 255, b = 255}, {x = 0, y = 0, z = 0})
+  MAPi.hangout_add_env_tint(hangout_madotsuki, {r = 255, g = 255, b = 255}, {x = 0, y = 0, z = 0})
   
   --adds a new hangout to mapi
-  hangout_poniko = _G.MAPi.hangout_map_add(
+  hangout_poniko = MAPi.hangout_map_add(
     LVL_PONIKO, 
     "Poniko's House",
     "The house of one of the NPCs from the game Yume Nikki! (it does the thing btw)",
@@ -38,18 +38,18 @@ if _G.MAPi_Active then
     })
   
   --Adds an entry sound to the level
-  _G.MAPi.hangout_add_entry_sound(hangout_poniko, ponikoenter)
+  MAPi.hangout_add_entry_sound(hangout_poniko, ponikoenter)
   
   --Adds an environment tint to the map so it can be modified later
-  _G.MAPi.hangout_add_env_tint(hangout_poniko, {r = 255, g = 255, b = 255}, {x = 0, y = 0, z = 0})
+  MAPi.hangout_add_env_tint(hangout_poniko, {r = 255, g = 255, b = 255}, {x = 0, y = 0, z = 0})
   
   --adds a non existent skybox to area 1 (poniko house) so it uses the default one set in blender (in this case pitch black)
-  _G.MAPi.hangout_add_skybox(hangout_poniko, {nil})
+  MAPi.hangout_add_skybox(hangout_poniko, {nil})
   
   MAPi.hangout_edit_text_color(hangout_poniko, {r = 0xAD, g = 0xD6, b = 0x5C})
 
   --adds a skybox to area 2 (uboa's trap) since it is the second time the function is called on the same hangout
-  _G.MAPi.hangout_add_skybox(hangout_poniko,
+  MAPi.hangout_add_skybox(hangout_poniko,
    {
     skytype = "ico",
     front = uboasky
